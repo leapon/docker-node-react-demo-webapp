324e24446d34902bfa3745fc852b8ca7060a3e3a
leapon <uestudio@gmail.com>
2015-07-31 00:09:49 -0400

    two pages added

--- site/app_modules/web.js
+++ site/app_modules/web.js
@@ -11,7 +11,19 @@ module.exports = function(app) {
     block.data = tool.object(require('basedata')(app, moduleName));
     block.page = tool.object(require('basepage')(app, moduleName, block.data));
     
+    block.page.getReference = function(req, res) {
+        var page = { moduleName:moduleName };
+        res.render(moduleName + '/reference', { page:page });
+    };
+    
+    block.page.getAbout = function(req, res) {
+        var page = { moduleName:moduleName };
+        res.render(moduleName + '/about', { page:page });
+    };
+    
     app.server.get('/', block.page.getIndex);
+    app.server.get('/reference', block.page.getReference);
+    app.server.get('/about', block.page.getAbout);
     app.server.get('/' + moduleName + '/page/:pagename', block.page.showPage);
     
     return block;
